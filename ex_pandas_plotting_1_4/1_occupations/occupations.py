e2cb1fb3143bf2f76e251353b68529a88bd339c3
Rosalesjulio <rosju77@gmail.com>
2020-08-28 11:07:54 +0200

    final occup

--- ex_pandas_plotting_1_4/1_occupations/occupations.py
+++ ex_pandas_plotting_1_4/1_occupations/occupations.py
@@ -5,25 +5,13 @@
 
 # ### 1.a Import the dataset 
 
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
 
 
 FNAME = "https://raw.githubusercontent.com/justmarkham/DAT8/master/data/u.user"
 
 
-# In[3]:
-
-
 df=pd.read_csv(FNAME, delimiter ='|')
-
-
-# In[4]:
 
 
 df.head(7)
@@ -31,41 +19,26 @@ df.head(7)
 
 # ### 1.b Print the last 10 entries and the first 25 entries
 
-# In[5]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-
-# In[6]:
-
 
 import matplotlib.pyplot as plt
-
-
-# In[7]:
 
 
 df.tail(10)
 
 
-# In[8]:
-
-
 df.head(25)
+
+
+df.describe()
 
 
 # ###  1.c Type of each column of the dataset 
 
-# In[ ]:
 
-
-
+df.dtypes
 
 
 # ### 1.d Age values with the least occurrence 
-
-# In[9]:
 
 
 df["age"].value_counts()
@@ -73,27 +46,30 @@ df["age"].value_counts()
 
 # ### 1.e Frecuency distribution occupation 
 
-# In[10]:
-
 
 df["occupation"].value_counts()
 
 
+# In[28]:
+
+
+occup_freq = df["occupation"].value_counts()
+
+
+occup_freq.head()
+
+
 # ### 1.f Type of the object created 
 
-# ********
+# #### the object created by = operator in this case is data labeled with the different categories of occupation 
 
 # ### 1.g Frecuency distribution occupation type 
-
-# In[11]:
 
 
 df["occupation"].value_counts()
 
 
 # ### 1.h Histogram for occupation 
-
-# In[17]:
 
 
 df['occupation'].value_counts().plot(kind='bar')
@@ -102,4 +78,6 @@ plt.xlabel ('types', fontsize = "medium")
 plt.ylabel('frecuency')
 plt.xticks(rotation='vertical')
 plt.savefig('occupation_types.pdf')
+
+
 
